69f17a0f8fa0593a9c81fbdc83582531b0343f7d
User <User@DESKTOP-2MLO5K4>
2025-03-07 19:50:27 +0100

    "Version inicial sin modificar"

---
+++ src/casuso_rubiales_alvaro_ed04_tarea/Casuso_Rubiales_Alvaro_ED04_Tarea.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casuso_rubiales_alvaro_ed04_tarea;
+
+/**
+ *
+ * @author User
+ */
+public class Casuso_Rubiales_Alvaro_ED04_Tarea {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+    }
+    
+
